9c11db85255dbdccf1e7816ab0d0f2a23d42e5a5
FirjaX <107788805+FirjaX@users.noreply.github.com>
2024-08-12 21:39:34 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,9 +5,9 @@ Replace Your Key with the apikey that you get in your profile.
 */
 
 module.exports = {
-  ownerUsernames: [""],//Your Username
-  ownerNumber: "",//Your Number 
-  telegramBotToken: "",//Your Bot Tokens
+  ownerUsernames: ["@vanestzy"],//Your Username
+  ownerNumber: "6282322125895",//Your Number 
+  telegramBotToken: "6384516529:AAF5yfeg2HVK3XeOgV1ilZw4V77kmurSIrs",//Your Bot Tokens
   imageUrl: "https://btch.pages.dev/file/313139c543db723fb67e6.jpg",//Thumbnail Url
   buttonUrl: "https://github.com/ERLANRAHMAT/telebot",//Script Url
   api: "https://api.betabotz.eu.org",
